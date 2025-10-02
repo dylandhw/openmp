1935af5363f8525bcde818f0bc60676abbb5412a
dylandhw <dylanwilkins2308@gmail.com>
2025-10-02 16:39:38 -0400

    switch

--- refresh/ctrlflw.cpp
+++ refresh/ctrlflw.cpp
@@ -4,10 +4,26 @@ using std::cout;
 using std::endl;
 
 int main() {
-    int a = 5;
+    int a = 4;
     int b = 10;
 
-    if (a == 5) cout << "Inside if statement (a == " << a << ")" << endl;
+    if (a == 5) {
+        cout << "Inside if statement (a == " << a << ")" << endl;
+    } else if (a == 6) {
+        cout << "A is not equal to 5, but it is equal to 6!" << endl;
+    } else {
+        cout << "A is not equal to either!" << endl;  
+    }
     
+    cout << "---Switch-Statements-----" << endl;
+    switch (a) {
+        case 4:
+            cout << a << " == 4" << endl;
+            break;
+        case 6:
+            cout << a << " == 6" << endl;
+            break;
+    }
+
     return 0;
 }
