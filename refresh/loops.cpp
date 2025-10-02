e208d2d31227d0d579b27959231a221284845551
dylandhw <dylanwilkins2308@gmail.com>
2025-10-02 17:57:18 -0400

    iterators

--- refresh/loops.cpp
+++ refresh/loops.cpp
@@ -9,8 +9,16 @@ int main(){
     array<int, 5> a1{0, 1, 2, 3, 4};
     
     cout << "-----Range-Based-----" << endl;
+    // Do not have to worry about indexing, for each element i in a1.
     for (auto i : a1) {
         cout << i << " ";
+        cout << a1[i] * 2 << endl;
+    }
+    cout << endl;
+
+    cout << "-----Iterators-----" << endl;
+    for (auto it = a1.begin(); it < end(a1); it++) {
+        cout << *it << endl;
     }
     cout << endl;
 }
