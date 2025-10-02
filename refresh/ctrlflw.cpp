99b44225fa4a32164f066856f68afae0354f8786
dylandhw <dylanwilkins2308@gmail.com>
2025-10-02 16:34:48 -0400

    ctrl

---
+++ refresh/ctrlflw.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    if (a == 5) cout << "Inside if statement (a == " << a << ")" << endl;
+    
+    return 0;
+}
