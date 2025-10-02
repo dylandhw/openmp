9391a029d41552b2b0045cf32e48b3fc4afbc200
dylandhw <dylanwilkins2308@gmail.com>
2025-10-02 18:16:22 -0400

    some stuff

--- refresh/vectors.cpp
+++ refresh/vectors.cpp
@@ -11,8 +11,18 @@ int main() {
 
     // Method for getting vector size
     cout << "Vector 1 size = " << v1.size() << endl;
-
     // Method for getting vector capacity
-    cout << "Vector 1 size = " << v1.capacity() << endl;
+    cout << "Vector 1 capacity = " << v1.capacity() << endl;
+
+    // Methods for putting into our vector
+    v1.push_back(1); // Less efficient, creates (or copies from) outside vector then copies it in
+    v1.emplace_back(2); // More efficient, done in place
+
+    v1.push_back(3); // Capacity is expanded exponentially, everytime we do push_back()
+    // We do not need to rely on exponential allocation though..
+    v1.reserve(3); // Gives space for 3 integers (only increases the size of our container)
+    cout << "Vector 1 size = " << v1.size() << endl;
+    cout << "Vector 1 capacity = " << v1.capacity() << endl;       
+
 
 }
