f82815ba4bd14d74ffe7a161299f70ff052bc141
dylandhw <dylanwilkins2308@gmail.com>
2025-10-02 16:18:56 -0400

    init

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// prints from multiple threads
+
+#include <iostream>
+#include <omp.h>
+
+int main(){
+#pragma omp parallel
+  {
+    std::cout << "Printing from thread " << omp_get_thread_num() << "\n"; 
+  }
+   
+    return 0;
+}
