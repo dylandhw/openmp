23be82661ab3f3ff6a9ade4b7992055895110421
dylandhw <dylanwilkins2308@gmail.com>
2025-10-04 18:46:59 -0400

    .

--- reimann.cpp
+++ reimann.cpp
@@ -6,5 +6,17 @@ using namespace std;
 #define PI acos(-1.0) // defining PI as the arccos of -1
 
 int main(int argc, char* argv[]) {
-    
+    int N = stoi(argv[1]);
+
+    double sum = 0;
+
+    #pragma omp parallel
+    {
+        #pragma omp for // for loop
+        for(int m = 0; m<N-1; m++)
+        {
+
+        }
+    }
+
 } 
