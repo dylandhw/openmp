0238658569c22bc71f21ae7b1fbb29afc5d5ce4c
dylandhw <dylanwilkins2308@gmail.com>
2025-10-04 18:45:51 -0400

    .

---
+++ reimann.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <omp.h>
+#include <math.h>
+
+using namespace std;
+#define PI acos(-1.0) // defining PI as the arccos of -1
+
+int main(int argc, char* argv[]) {
+    
+} 
